957bca410e061b7c901790eccc627a7629b039e2
Evgenii Buslaev <eugenebus144@gmail.com>
2022-06-30 15:00:26 +0500

    styled html

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const input = document.querySelector(".new-task-input");
 const addButton = document.querySelector(".new-task-submit");
 const addedTasksContainer = document.querySelector(".container-output");
 const addedTask = document.querySelector(".added-tasks");
+const deadLine = document.querySelector(".new-task-deadline-input");
 
 let monthArray = [
   "Jan",
@@ -65,10 +66,10 @@ addButton.addEventListener("click", () => {
 
       // working with time
       const editDate = new Date();
-      const editDay = date.getDate();
-      const editMonth = date.getMonth();
-      const editHour = date.getHours();
-      const editMinutes = date.getMinutes();
+      const editDay = editDate.getDate();
+      const editMonth = editDate.getMonth();
+      const editHour = editDate.getHours();
+      const editMinutes = editDate.getMinutes();
 
       let editStr = `Edited on ${editDay} ${monthArray[editMonth]} at ${editHour}:${editMinutes}`;
       dateDiv.innerText = editStr;
@@ -82,6 +83,7 @@ addButton.addEventListener("click", () => {
   });
 
   input.value = "";
+
   // working with the time
   const date = new Date();
   const day = date.getDate();
@@ -90,10 +92,44 @@ addButton.addEventListener("click", () => {
   const minutes = date.getMinutes();
 
   const str = `Created on ${day} ${monthArray[month]} at ${hour}:${minutes}`;
+  let dateTimerCont = document.createElement("div");
+  dateTimerCont.classList.add("dates");
 
   let dateDiv = document.createElement("div");
   dateDiv.classList.add("date");
 
   dateDiv.innerText = str;
-  addedTasksContainer.append(dateDiv);
+  dateTimerCont.appendChild(dateDiv);
+  addedTasksContainer.append(dateTimerCont);
+
+  // wordking with deadlines
+  if (deadLine.value <= 100 && deadLine.value > 0) {
+    let divDeadLine = document.createElement("div");
+    divDeadLine.classList.add("date");
+    divDeadLine.innerText = "00:00:00 left";
+    dateTimerCont.appendChild(divDeadLine);
+  } else {
+    if (deadLine.value > 100)
+      alert(
+        `Please, add your task with deadline later. You have enough time yet`
+      );
+    if (deadLine.value < 0) alert(`Wrong integer`);
+  }
+  deadLine.value = "";
 });
+
+function deadlineTimer(s) {
+  s--;
+  if (h.length < 2) {
+    h = "0" + h;
+  }
+  if (m.length < 2) {
+    m = "0" + m;
+  }
+  if (s.length < 2) {
+    s = "0" + s;
+  }
+
+  let clockString = h + ":" + m + ":" + s;
+  return clockString;
+}
